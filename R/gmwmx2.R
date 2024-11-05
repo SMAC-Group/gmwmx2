164999429b3fbe16319ef5f893a41d3dc5ad461b
Lionel Voirol <lionelvoirol@hotmail.com>
2024-11-05 17:50:19 +0100

    implemented function gmwmx2 and plot.fit_gnss_ts_ngl and started writing vignette for eestimation

--- R/gmwmx2.R
+++ R/gmwmx2.R
@@ -1,8 +1,415 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-## usethis namespace: start
-#' @useDynLib gmwmx2, .registration = TRUE
-## usethis namespace: end
-NULL
+create_X_matrix = function (all_mjd_index,
+                            jumps,
+                            n_seasonal,
+                            vec_earthquakes_index_mjd,
+                            vec_earthquakes_relaxation_time) {
+  # all_stations = download_all_stations_names_ngl()
+  # x = download_station_ngl("AGDS")
+  # x$df_equipment_software_changes
+  # x$df_earthquakes
+  # dim(x$df_position)
+  # x$df_earthquakes
+  #
+  # # create full index
+  # all_mjd_index = seq(head(x$df_position$modified_julian_day,1), tail(x$df_position$modified_julian_day, 1))
+  # jumps = c(x$df_equipment_software_changes$modified_julian_date,x$df_earthquakes$modified_julian_date )
+  # # remove jumps that are after or before the last/first index
+  # jumps_inside_interval_mjd = dplyr::between(jumps, left = head(x$df_position$modified_julian_day,1), right = tail(x$df_position$modified_julian_day,1))
+  # jumps= jumps[jumps_inside_interval_mjd]
+  # vec_earthquakes_index_mjd = c(x$df_earthquakes$modified_julian_date)
+  # n_seasonal = 2
+
+  # ensure that vec_earthquakes_relaxation_time is the same length as vec_earthquakes_index_mjd if not NULL
+  if(!is.null(vec_earthquakes_relaxation_time)){
+    if(length(vec_earthquakes_index_mjd) != length(vec_earthquakes_relaxation_time)){
+      stop("Vector of relaxation time not of same length as vector of MJD index of earthquakes")
+    }
+  }
+
+
+  # set to default 1 year if no earthquakes relaxation time provided
+  if(is.null(vec_earthquakes_relaxation_time)){
+    vec_earthquakes_relaxation_time = rep(365.25, length(vec_earthquakes_index_mjd))
+  }
+
+
+  # create empty matrix
+  X = matrix(0, nrow = length(all_mjd_index), ncol = 2 + 2 * n_seasonal +
+               length(jumps) + length(vec_earthquakes_index_mjd))
+
+  # add bias, intercept
+  X[, 1] = 1
+
+  X[,2] = all_mjd_index
+
+
+  # add seasonal
+  if (n_seasonal > 0) {
+    for (i in 1:n_seasonal) {
+      X[, 2 + (i - 1) * 2 + 1] = sin((all_mjd_index - 51544) *  i * 2 * pi/365.25)
+      X[, 2 + (i - 1) * 2 + 2] = cos((all_mjd_index - 51544) * i * 2 * pi/365.25)
+    }
+  }
+
+  # add offsets
+  if (!is.null(jumps)) {
+    for (i in 1:length(jumps)) {
+      # i = 2
+      it = min(which(all_mjd_index > jumps[i] - 1e-06))
+      X[, 2 + 2 * n_seasonal + i] = c(rep(0, it - 1), rep(1,
+                                                          length(all_mjd_index) - it + 1))
+    }
+  }
+
+  # exponential decay function for post seismic relaxation
+  if(!is.null(vec_earthquakes_index_mjd)){
+    for(i in seq_along(vec_earthquakes_index_mjd)){
+      tau_i = vec_relaxation_time[i]
+      earthquake_mjd_i = vec_earthquakes_index_mjd[i]
+      # create column in matrix
+      X[, 2 + 2 * n_seasonal + length(jumps) + i] = 1-exp(-(all_mjd_index- earthquake_mjd_i)/tau_i)
+    }
+  }
+
+
+  # # Slow slip events (tanh)
+  # if(!is.null(vec_tanh_mid_point)){
+  #   for(i in seq_along(vec_tanh_mid_point)){
+  #     # i = 1
+  #     t_k = vec_tanh_mid_point[i]
+  #     T_k = vec_tanh_length[i]
+  #     X[, 2 + 2 * n_seasonal + length(jumps) + i] = 0.5 * (tanh((t_nogap - t_k)/T_k) -1)
+  #   }
+  # }
+  rownames(X) = all_mjd_index
+  return(X)
+}
+
+
+# define optimization function
+objective_function_wn_flicker_w_missing <- function(theta, wv_obj, n, quantities_D, approx_type, vec_autocov_omega, pstar_hat, no_missing=T, omega = NULL) {
+  theta_t <- vector(mode = "numeric", length = 2)
+  theta_t[1] <- exp(theta[1]) # sigma2 wn
+  theta_t[2] <- exp(theta[2]) # sigma2 fl
+
+  vec_mean_autocov <- vec_mean_autocov_powerlaw(kappa = -1, n) *  theta_t[2]
+  vec_mean_autocov[1] <- vec_mean_autocov[1] +   theta_t[1]
+
+  # approx with linear interpolation on errors
+  vec_mean_autocov_eps_hat <- compute_all_mean_diag_fast_w_linear_interp_only_required_cpp(
+    mat_D_q_term_1 = quantities_D$mat_D_q_term_1,
+    mat_D_q_term_2 = quantities_D$mat_D_q_term_2,
+    sum_on_sub_diag_of_D = quantities_D$sum_on_sub_diag_of_D,
+    vec_autocov = vec_mean_autocov, approx_type = approx_type
+  )
+  if(!no_missing){
+    vec_mean_per_diag_w_missing = vec_mean_autocov_eps_hat * ( vec_autocov_omega + pstar_hat^2)
+    theo_wv <- autocovariance_to_wv(vec_mean_per_diag_w_missing, tau = wv_obj$scales)
+
+  }else{
+    theo_wv <- autocovariance_to_wv(vec_mean_autocov_eps_hat, tau = wv_obj$scales)
+
+  }
+  nu_hat <- wv_obj$variance
+  if (is.null(omega)) {
+    omega <- diag(1 / (wv_obj$ci_low - wv_obj$ci_high)^2)
+  }
+
+  difference <- nu_hat - theo_wv
+  objective <- t(difference) %*% omega %*% difference
+  return(objective)
+}
+
+
+
+#' Estimate a linear model with White noise and Flicker noise for the residuals in presence of missing data using the GMWMX estimator.
+#' @importFrom wv wvar
+#' @importFrom dplyr between
+#' @importFrom Matrix solve
+#' @export
+gmwmx2 = function(x, n_seasonal, vec_earthquakes_relaxation_time = NULL, component ="N"){
+
+  # all_stations = download_all_stations_names_ngl()
+  # x = download_station_ngl("0AMB")
+  # dim(x$df_position)
+  # x$df_earthquakes
+  # x$df_equipment_software_changes
+  # n_seasonal=2
+  # vec_earthquakes_relaxation_time = NULL
+  # component = "E"
+
+  # check that component is either N, E or V
+  if(!component %in% c("N", "E", "V")){
+    stop("Specified component should be either 'N', 'E' or 'V'")
+  }
+
+  # create full index
+  all_mjd_index = seq(head(x$df_position$modified_julian_day,1), tail(x$df_position$modified_julian_day, 1))
+
+  # create all jumps by combining jumps due to equipment change and jumps due to earthquakes
+  jumps = c(x$df_equipment_software_changes$modified_julian_date,
+            x$df_earthquakes$modified_julian_date )
+
+  # # remove jumps that are after or before the last/first index
+  jumps_inside_interval_mjd = dplyr::between(jumps, left = head(x$df_position$modified_julian_day,1), right = tail(x$df_position$modified_julian_day,1))
+  jumps = jumps[jumps_inside_interval_mjd]
+  vec_earthquakes_index_mjd = c(x$df_earthquakes$modified_julian_date)
+
+  if(length(jumps) == 0){
+    jumps=NULL
+  }
+
+  # create matrix
+  X = create_X_matrix(all_mjd_index = all_mjd_index,
+                      jumps = jumps,
+                      n_seasonal = n_seasonal,
+                      vec_earthquakes_index_mjd = vec_earthquakes_index_mjd,
+                      vec_earthquakes_relaxation_time = vec_earthquakes_relaxation_time )
+
+
+  # obtain X_sub
+  id_X_sub = which(rownames(X) %in% x$df_position$modified_julian_day)
+  X_sub = X[id_X_sub,]
+
+  # Extract Y given specified component
+  if(component == "N"){
+    y = x$df_position$northings_fractional_portion
+  }else if (component == "E"){
+    y = x$df_position$eastings_fractional_portion
+  }else if(component == "V"){
+    y = x$df_position$vertical_fractional_portion
+  }
+
+  # obtain beta hat
+  beta_hat <- .lm.fit(y = y , x = X_sub)$coefficients
+
+  # # plot signal and estimated model
+  # plot(x=X_sub[,2], y=y, type="l")
+  # lines(x=X_sub[, 2], y= X_sub%*% beta_hat, col="red")
+  # # obtain observed residuals
+  eps_hat_sub <- y - X_sub %*% beta_hat
+
+  # create
+  eps_hat_filled <- vector(mode = "numeric", length = length(all_mjd_index))
+
+  # fill in observed residuals
+  eps_hat_filled[id_X_sub] = eps_hat_sub
+
+  # compute empirical wv variance on the "filled" vector of residuals
+  wv_emp_eps_hat_filled <- wv::wvar(eps_hat_filled)
+
+  # plot(wv_emp_eps_hat_filled)
+  # define vector of observed index
+  vec_omega = as.numeric(all_mjd_index %in% x$df_position$modified_julian_day )
+
+  # estimate parameter of Markov process missingness process using the MLE
+  p_hat <- estimate_p1_p2_mle_cpp(vec_omega)
+
+  pstar_hat <- p_hat[2] / (p_hat[1] + p_hat[2])
+
+  # get vec autocovariance theo omega
+  vec_autocov_omega <- create_vec_theo_autocov_omega_cpp(p1 = p_hat[1], p2 = p_hat[2], length(all_mjd_index))
+
+  H <- X %*% solve(t(X) %*% X) %*% t(X)
+  D <- diag(length(all_mjd_index)) - H
+
+  # precompute quantities on D
+  quantities_D <- pre_compute_quantities_on_D_only_required_smarter_cpp(D, approx_type = "3")
+
+  # define missingness case
+  if (all(vec_omega == 1)) {
+    no_missing <- T
+  } else {
+    no_missing <- F
+  }
+
+
+  # define gamma init
+  gamma_init = c(log(8e-7), log(8e-9)) # arbitrary values around the ones observed in practice for these kind of data
+
+  # fit gmwmx on empirical wv
+  res_gmwmx <- optim(
+    par = gamma_init,
+    fn = objective_function_wn_flicker_w_missing,
+    wv_obj = wv_emp_eps_hat_filled,
+    n = length(vec_omega),
+    quantities_D = quantities_D,
+    approx_type = "3",
+    vec_autocov_omega = vec_autocov_omega,
+    pstar_hat = pstar_hat,
+    no_missing = no_missing
+  )
+
+  # res_gmwmx
+  gamma_hat_1 = exp(res_gmwmx$par)
+
+  # construct sigma matrix of white noise + ficker
+  var_cov_mat_wn = gamma_hat_1[1] * diag(length(vec_omega))
+  var_cov_mat_flicker = var_cov_powerlaw_cpp(sigma2 = gamma_hat_1[2], kappa = -1, n = length(vec_omega))
+  var_cov_mat_epsilon = var_cov_mat_wn+var_cov_mat_flicker
+
+  # get var cov missingness process
+  var_cov_omega = toeplitz(as.vector(vec_autocov_omega))
+
+  # define variance covariance of residuals with missing
+  var_cov_eps_hat_w_missing = var_cov_mat_epsilon * ( var_cov_omega + pstar_hat^2)
+
+  # compute variance of wavelet variance computed on epsilon (negleting the fact that we compute the wv on the "estimated" residuals)
+  var_cov_nu_hat = compute_cov_wv_cpp_approx_faster(Sigma_X = var_cov_eps_hat_w_missing)
+
+  # get inverse
+  inv_var_cov_nu_hat =  Matrix::solve(var_cov_nu_hat)
+
+  # re estimate gamma with optimal weighting matrix
+  res_gmwmx_2 <- optim(
+    par = res_gmwmx$par,
+    fn = objective_function_wn_flicker_w_missing,
+    wv_obj = wv_emp_eps_hat_filled,
+    n = length(vec_omega),
+    quantities_D = quantities_D,
+    approx_type = "3",
+    vec_autocov_omega = vec_autocov_omega,
+    omega = inv_var_cov_nu_hat,
+    pstar_hat = pstar_hat,
+    no_missing = no_missing
+  )
+
+  gamma_hat_2 = exp(res_gmwmx_2$par)
+
+  # get theo wv with approximation obtained by last fit
+  vec_mean_autocov <- vec_mean_autocov_powerlaw(kappa = -1, length(vec_omega)) *  gamma_hat_2[2]
+  vec_mean_autocov[1] <- vec_mean_autocov[1] +   gamma_hat_2[1]
+
+  # approx with linear interpolation on errors
+  vec_mean_autocov_eps_hat <- compute_all_mean_diag_fast_w_linear_interp_only_required_cpp(
+    mat_D_q_term_1 = quantities_D$mat_D_q_term_1,
+    mat_D_q_term_2 = quantities_D$mat_D_q_term_2,
+    sum_on_sub_diag_of_D = quantities_D$sum_on_sub_diag_of_D,
+    vec_autocov = vec_mean_autocov, approx_type = "3"
+  )
+  if(!no_missing){
+    vec_mean_per_diag_w_missing = vec_mean_autocov_eps_hat * ( vec_autocov_omega + pstar_hat^2)
+    theo_wv <- autocovariance_to_wv(vec_mean_per_diag_w_missing, tau = wv_emp_eps_hat_filled$scales)
+
+  }else{
+    theo_wv <- autocovariance_to_wv(vec_mean_autocov_eps_hat, tau = wv_emp_eps_hat_filled$scales)
+
+  }
+
+
+
+  # Compute variance covariance of beta hat
+  if (no_missing) {
+    XtX_inv <- Matrix::solve(t(X) %*% X)
+    var_cov_beta_hat = XtX_inv %*% t(X) %*% var_cov_mat_epsilon  %*% X %*% XtX_inv
+  } else {
+    XtX_inv <- Matrix::solve(t(X) %*% X)
+    var_cov_beta_hat <- pstar_hat^(-2) * XtX_inv %*% t(X) %*% ((var_cov_omega + pstar_hat^2) * var_cov_mat_epsilon) %*% X %*% XtX_inv
+  }
+
+  std_beta_hat_gmwmx_3 <- sqrt(diag(var_cov_beta_hat))
+
+  ret = list("beta_hat" = beta_hat,
+             "std_beta_hat" = std_beta_hat_gmwmx_3,
+             "gamma_hat" = gamma_hat_2,
+             "vartheta_hat" = p_hat,
+             "component" = component,
+             "design_matrix_X" = X_sub,
+             "y" = y,
+             "empirical_wvar" = wv_emp_eps_hat_filled,
+             "theoretical_wvar" =theo_wv,
+             "df_position" = x$df_position,
+             "df_earthquakes" = x$df_earthquakes,
+             "df_equipment_software_changes" = x$df_equipment_software_changes)
+
+  class(ret) = "fit_gnss_ts_ngl"
+
+  return(ret)
+
+
+}
+
+
+
+
+
+
+
+#' Plot a \code{fit_gnss_ts_ngl} object
+#' @export
+#' @importFrom wv plot.wvar
+#' @return No return value. Plot a \code{fit_gnss_ts_ngl} object.
+plot.fit_gnss_ts_ngl = function(x){
+  #
+  # #
+  # library(gmwmx2)
+  # station_data = download_station_ngl("0AMB")
+  # x = gmwmx2(station_data, n_seasonal = 2, component = "N")
+
+  # Save the current graphical parameters
+  old_par <- par(no.readonly = TRUE)
+
+  # set parameters for layout
+  mat_layout = matrix(c(1,2, 3),ncol=1, nrow=3)
+  layout(mat_layout, heights = c(.1,1, 1))
+  par(mar=c(0,0,0,0))
+  plot.new()
+  legend("center", horiz = T,
+         legend = c("NA", "Equipment/Software change", "Earthquake", "Estimated fit"),
+         col = c("grey60", "blue", "darkorange", "red"),
+         pch = c(15, NA, NA, NA),
+         pt.cex=c(2, NA, NA, NA),
+         # x.intersp = 0.8,
+         text.width = c(.1,.3,.2,.1)  ,
+         lty=c(NA, 1,1,1), bty="n")
+  par(mar=c(4, 4.1, 2, 2.1))
+
+
+  component = x$component
+  if(component == "E"){
+    axis_name = "Easting (m)"
+  }else if(component == "N"){
+    axis_name = "Northing (m)"
+  }else if(component == "V"){
+    axis_name = "Vertical (m)"
+  }
+
+  # plot data
+  plot(x$design_matrix_X[,2], x$y, type="l", las=1,ylab=axis_name, xlab="MJD")
+
+  # add estimated fit
+  lines(x=x$design_matrix_X[,2], y= x$design_matrix_X%*% x$beta_hat, col="red")
+
+  # compute NA over the time series
+  all_mjd = seq(head(x$df_position$modified_julian_day,1),
+                tail(x$df_position$modified_julian_day,1))
+  missing_mjd = all_mjd[which(!all_mjd %in% x$df_position$modified_julian_day)]
+
+  # missing data
+  for(i in seq_along(missing_mjd)){
+    abline(v = missing_mjd[i], col="grey60")
+  }
+
+  # add equipment change
+  for(i in seq((dim(x$df_equipment_software_changes)[1]))){
+    abline(v = x$df_equipment_software_changes$modified_julian_date, col="blue")
+  }
+
+  # add earthquake
+  for(i in seq((dim(x$df_earthquakes)[1]))){
+    abline(v = x$df_earthquakes$modified_julian_date, col="darkorange")
+  }
+  box()
+
+  # plot wvar
+  plot(x$empirical_wvar)
+  # add theoretical implied wv
+  lines(x=x$empirical_wvar$scales, y=x$theoretical_wvar, col="red")
+  box()
+
+  # Restore the original graphical parameters
+  par(old_par)
+
+  # Reset to a single plot layout
+  layout(1)
+}
--- src/flicker_2.cpp
+++ src/flicker_2.cpp
@@ -5,152 +5,6 @@ using namespace Rcpp;
 using namespace arma;
 
 
-
-
-// [[Rcpp::export]]
-arma::vec gen_flicker(int N, double sigma) {
-  double kappa=-1;
-  
-  // // Set seed for reproducibility
-  // arma::arma_rng::set_seed(0);
-  
-  // Flicker noise parameters
-  double h_prev = 1.0;
-  arma::vec h = arma::zeros<arma::vec>(2 * N);
-  h(0) = 1.0;  // Eq. (25)
-  
-  // Generate h values
-  for (int i = 1; i < N; ++i) {
-    h(i) = (i - kappa / 2 - 1) / static_cast<double>(i) * h_prev;  // Eq. (25)
-    h_prev = h(i);
-  }
-  
-  // Generate v values
-  arma::vec v = arma::zeros<arma::vec>(2 * N);
-  v.subvec(0, N - 1) = arma::randn<arma::vec>(N) * sigma;
-  
-  // Compute w values
-  arma::cx_vec fft_v = arma::fft(v);
-  arma::cx_vec fft_h = arma::fft(h);
-  arma::cx_vec w1 = arma::ifft(fft_v % fft_h);
-  
-  // Extract real part of w
-  arma::vec w2 = arma::real(arma::vectorise(w1));
-  
-  // Extract the first N elements of w
-  arma::vec y = w2.subvec(0, N - 1);
-  
-  return y;
-}
-
-
-
-
-
-
-// function to compute cov mat of flicker
-// [[Rcpp::export]]
-arma::mat create_var_cov_flicker(double sigma_pl,  int N) {
-  // use var_cov_powerlaw_cpp, more general and faster !
-  double kappa = -1;
-  arma::mat U(N, N, arma::fill::eye); 
-  
-  arma::vec h(N);
-  double h_prev = 1.0;
-  
-  for (int i = 1; i < N; ++i) {
-    h(i) = (i - kappa / 2 - 1) / static_cast<double>(i) * h_prev; // Eq. (25)
-    for (int j = 0; j < N - i; ++j) {
-      U(j, j + i) = h(i);
-    }
-    h_prev = h(i);
-  }
-  
-  U *= sigma_pl; // scale noise
-  // long huge matrix multiplicaiton
-  return trans(U) * U; // Eq. (26)
-}
-
-
-// [[Rcpp::export]]
-arma::mat create_U_flicker(int N) {
-  double kappa = -1;
-  arma::mat U(N, N, arma::fill::eye); 
-  
-  arma::vec h(N);
-  double h_prev = 1.0;
-  
-  for (int i = 1; i < N; ++i) {
-    h(i) = (i - kappa / 2 - 1) / static_cast<double>(i) * h_prev; // Eq. (25)
-    for (int j = 0; j < N - i; ++j) {
-      U(j, j + i) = h(i);
-    }
-    h_prev = h(i);
-  }
-  
-  // U *= sigma_pl; // scale noise
-  
-  // return trans(U) * U; // Eq. (26)
-  return(U);
-}
-
-
-// Function to calculate mean of all diagonals of a matrix
-// [[Rcpp::export]]
-arma::vec mean_all_diagonals(arma::mat matrix) {
-  
-  // Ensure that the input is a square matrix
-  if (matrix.n_rows != matrix.n_cols) {
-    throw std::invalid_argument("Input must be a square matrix");
-  }
-  
-  // Get the size of the matrix
-  int size = matrix.n_rows;
-  
-  // Vector to store mean values for each diagonal
-  arma::vec diagonal_means(size, arma::fill::zeros);
-  
-  // Iterate over each diagonal
-  for (int i = 0; i < size; ++i) {
-    arma::vec diagonal_1 = diagvec(matrix, i);
-    arma::vec diagonal_2 = diagvec(matrix, -i);
-    arma::vec diagonal = join_cols(diagonal_1, diagonal_2);
-    double diagonal_mean = arma::mean(diagonal);
-    diagonal_means(i) = diagonal_mean;
-  }
-  
-  return diagonal_means;
-}
-
-
-
-// Function to calculate mean of all diagonals of a matrix
-// [[Rcpp::export]]
-arma::vec mean_all_upper_diagonals(arma::mat matrix) {
-  
-  // Ensure that the input is a square matrix
-  if (matrix.n_rows != matrix.n_cols) {
-    throw std::invalid_argument("Input must be a square matrix");
-  }
-  
-  // Get the size of the matrix
-  int size = matrix.n_rows;
-  
-  // Vector to store mean values for each diagonal
-  arma::vec diagonal_means(size, arma::fill::zeros);
-  
-  // Iterate over each diagonal
-  for (int i = 0; i < size; ++i) {
-    arma::vec diagonal_1 = diagvec(matrix, i);
-    double diagonal_mean = arma::mean(diagonal_1);
-    diagonal_means(i) = diagonal_mean;
-  }
-  
-  return diagonal_means;
-}
-
-
-
 // [[Rcpp::export]]
 arma::vec compute_h_cpp(double kappa, int N) {
   arma::vec vec_h(N);
@@ -161,49 +15,20 @@ arma::vec compute_h_cpp(double kappa, int N) {
     vec_h(i) = h;
     h_prev = h;
   }
-  
+
   vec_h(0) = 1.0;
   return vec_h;
 }
 
-
 // [[Rcpp::export]]
-arma::vec return_cumsum_product_of_shifted_vector_cpp(const arma::vec& vec_x, int shift) {
-  int n = vec_x.n_elem;
-  arma::vec vec_y = vec_x;
-  arma::vec vec_x_tail = vec_x.tail(n - shift);
-  arma::vec vec_y_head = vec_y.head(n - shift);
-  arma::vec result = arma::cumsum(vec_x_tail % vec_y_head);
-  return result;
-}
-
-
-
-// Function to compute vec_mean_autocov_per_diag
-// [[Rcpp::export]]
-arma::vec vec_mean_autocov_per_diag_cpp( double kappa, int N) {
+arma::vec vec_mean_autocov_powerlaw( double kappa, int N) {
   arma::vec vec_h = compute_h_cpp(kappa, N);
   int n = vec_h.n_elem;
-  
-  arma::vec vec_mean_autocov_half(n);
-  vec_mean_autocov_half(0) = arma::mean(arma::cumsum(vec_h % vec_h));
-  
-  for (int i = 1; i < n; ++i) {
-    vec_mean_autocov_half(i) = arma::mean(return_cumsum_product_of_shifted_vector_cpp(vec_h, i));
-  }
-  return vec_mean_autocov_half;
-}
 
-
-// [[Rcpp::export]]
-arma::vec vec_mean_autocov_per_diag_2_cpp( double kappa, int N) {
-  arma::vec vec_h = compute_h_cpp(kappa, N);
-  int n = vec_h.n_elem;
-  
   // Initialize variables
   arma::vec vec_average_autocov2(n, arma::fill::zeros);
   // vec_average_autocov2[0] = val_sum / n;
-  
+
   // pass through diagonals
   for (int k = 0; k < n ; ++k) {
     double val_sum = 0.0;
@@ -219,54 +44,41 @@ arma::vec vec_mean_autocov_per_diag_2_cpp( double kappa, int N) {
   return(vec_average_autocov2);
 }
 
-// [[Rcpp::depends(RcppArmadillo)]]
+
 // [[Rcpp::export]]
 arma::mat var_cov_powerlaw_cpp(double sigma2, double kappa, int n) {
   arma::vec vec_h = compute_h_cpp(kappa, n);
   arma::mat sigma_mat(n, n, arma::fill::zeros);
   arma::vec vec_diag_k(n);
-  
+
   //  for each sub diagonal up to the upper right corner
   for (int k = 0; k < n; ++k) {
-    // re initialize sum 
+    // re initialize sum
     double val = 0.0;
-    
+
     // pass throught sum of product of h_{i} and h_{i+k}
     for (int i = 0; i < (n - k); ++i) {
       val += vec_h(i) * vec_h(i + k);
       vec_diag_k(i) = val;
     }
-    
+
     // Assign values to the upper diagonal
     sigma_mat.diag(k) = vec_diag_k.head(n - k);
   }
-  
+
   // multiply by sigma2
   sigma_mat = sigma_mat * sigma2;
-  
+
   // Create symmetric matrix from upper triangular matrix
   arma::mat symmetric_matrix = arma::symmatu(sigma_mat);
-  
+
   // // Create symmetric matrix
   // arma::mat symmetric_matrix = sigma_mat + sigma_mat.t();
   // // divide per two the value on the main diagonal are they are summed up two times in the previous operation
   // symmetric_matrix.diag() *= 0.5;
-  
+
   return symmetric_matrix;
 }
-
-// [[Rcpp::export]]
-arma::vec wn_autocovariance(double sigma2_wn, int n) {
-  arma::vec acf(n, fill::zeros);  // Create a vector of length n filled with zeros
-  acf(0) = sigma2_wn;            // Set the first element to sigma2_wn
-  return acf;
-}
-// Function to generate a Toeplitz matrix from a vector
-// [[Rcpp::export]]
-arma::mat fast_toeplitz_matrix_from_vector_cpp(const arma::vec& v) {
-  return arma::toeplitz(v);
-}
-
 
 
 // [[Rcpp::export]]
@@ -287,110 +99,30 @@ arma::vec compute_power_of_a_base(int x, int J) {
 arma::vec autocovariance_to_wv(const arma::vec& acf, const arma::vec& tau) {
   // Compute max scale
   double J = std::log10(tau(tau.n_elem - 1)) / std::log10(2.0);
-  
+
   // Index first element of acf
   double var_process = acf(0);
   arma::vec autocorr = acf.subvec(1, acf.n_elem - 1) / var_process;
   arma::vec ms = compute_power_of_a_base(2, J);
-  
+
   // Initialize vector for theoretical wavelet variance
   arma::vec theo_wv(J);
-  
+
   for (int j = 1; j <= J; ++j) {
     double m = ms(j - 1);
     double inter = m * (1 - autocorr(m - 1));
-    
+
     if (m > 1) {
       for (int i = 1; i <= m - 1; ++i) {
         inter += i * (2 * autocorr(m - i - 1) - autocorr(i - 1) - autocorr(2 * m - i - 1));
       }
     }
-    
+
     theo_wv(j - 1) = inter / (m * m) * var_process / 2.0;
   }
-  
+
   return theo_wv;
 }
-
-
-
-
-// [[Rcpp::export]]
-arma::vec get_mean_per_diag_I_minus_H_Sigma_cpp_4(const arma::mat& mat_I_minus_H, const arma::mat& mat_Sigma) {
-  // Get n
-  int n = mat_I_minus_H.n_rows;
-  
-  // get max J
-  int max_j = std::floor(log2(n))-1;
-  
-  // get max mean autocov to get
-  int max_index_mean_autocov_to_compute = 2 * std::pow(2, max_j - 1) - 1;
-  
-  // Create vector
-  arma::vec vec_mean_autocov_per_diag((max_index_mean_autocov_to_compute + 1), arma::fill::zeros);
-  
-  for (int i = 0; i <= max_index_mean_autocov_to_compute; ++i) {
-    // Compute the sum and mean for the current diagonal
-    vec_mean_autocov_per_diag(i) = arma::accu( mat_I_minus_H.submat(0, 0, n - i - 1, n - 1) % mat_Sigma.submat(i, 0, n - 1, n - 1)) / (n-i);
-  }
-  return vec_mean_autocov_per_diag;
-}
-
-
-// [[Rcpp::export]]
-arma::vec objective_function_wn_flicker_eps_hat_cpp(const arma::vec theta,const arma::vec scales, const arma::vec wv_var, const arma::vec wv_ci_up, const arma::vec wv_ci_low, const int n, const arma::mat& Q_matrix) {
-  double sigma2_wn = exp(theta(0));
-  double sigma2_fl = exp(theta(1));
-  
-  arma::vec wn_autocov_vec = wn_autocovariance(sigma2_wn, n);
-  arma::mat sigma_mat_wn = fast_toeplitz_matrix_from_vector_cpp(wn_autocov_vec); //sigma White Noise
-  
-  arma::mat sigma_mat_fl = var_cov_powerlaw_cpp(sigma2_fl,-1,n); //Sigma Flicker Noise
-  
-  arma::mat sigma_mat = sigma_mat_wn + sigma_mat_fl; //Sum both process
-  
-  arma::vec mean_autocov_eps_hat_vec = get_mean_per_diag_I_minus_H_Sigma_cpp_4(Q_matrix, sigma_mat);
-  
-  arma::vec theo_wv = autocovariance_to_wv(mean_autocov_eps_hat_vec, scales);
-  arma::vec variance = wv_var;
-  arma::vec ci_diff = wv_ci_low - wv_ci_up;
-  arma::mat omega = diagmat(1 / square(ci_diff));
-  arma::vec difference = variance - theo_wv;
-  arma::vec objective = difference.t() * omega * difference;
-  return objective ;
-}
-
-
-// [[Rcpp::export]]
-arma::vec objective_function_wn_flicker_eps_hat_cpp_2(const arma::vec theta,const arma::vec scales, const arma::vec wv_var, const arma::vec wv_ci_up, const arma::vec wv_ci_low, const int n, const arma::mat& Q_matrix) {
-  double sigma2_wn = exp(theta(0));
-  double sigma2_fl = exp(theta(1));
-  
-  arma::vec wn_autocov_vec = wn_autocovariance(sigma2_wn, n);
-  arma::mat sigma_mat_wn = fast_toeplitz_matrix_from_vector_cpp(wn_autocov_vec);
-  
-  arma::mat sigma_mat_fl = var_cov_powerlaw_cpp(sigma2_fl,-1,n);
-  
-  arma::mat sigma_mat = sigma_mat_wn + sigma_mat_fl;
-  
-  arma::mat sigma_mat_eps_hat = Q_matrix * sigma_mat * Q_matrix.t();
-  arma::vec mean_autocov_eps_hat_vec = mean_all_diagonals(sigma_mat_eps_hat);
-  
-  arma::vec theo_wv = autocovariance_to_wv(mean_autocov_eps_hat_vec, scales);
-  arma::vec variance = wv_var;
-  arma::vec ci_diff = wv_ci_low - wv_ci_up;
-  arma::mat omega = diagmat(1 / square(ci_diff));
-  arma::vec difference = variance - theo_wv;
-  arma::vec objective = difference.t() * omega * difference;
-  return objective ;
-}
-
-
-
-
-
-
-
 
 
 
